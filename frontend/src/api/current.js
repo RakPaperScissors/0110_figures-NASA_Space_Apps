e9bf5289531d45d838a13a2b2f7b56239079238c
Joseph Francis <jdbuhayan@up.edu.ph>
2025-10-04 13:17:11 +0800

    update

--- frontend/src/api/current.js
+++ frontend/src/api/current.js
@@ -5,16 +5,35 @@ const password = process.env.VITE_API_PASSWORD;
 const latitude = '7.0647';
 const longitude = '125.6088';
 
+function calculateHeatIndex(temperatureC, relativeHumidity) {
+  if (temperatureC <= 26.7) {
+    return temperatureC;
+  }
+
+  const tempF = (temperatureC * 9/5) + 32;
+
+  let heatIndexF = -42.379 + 2.04901523 * tempF + 10.14333127 * relativeHumidity
+                   - 0.22475541 * tempF * relativeHumidity - 0.00683783 * tempF * tempF
+                   - 0.05481717 * relativeHumidity * relativeHumidity
+                   + 0.00122874 * tempF * tempF * relativeHumidity
+                   + 0.00085282 * tempF * relativeHumidity * relativeHumidity
+                   - 0.00000199 * tempF * tempF * relativeHumidity * relativeHumidity;
+
+  const heatIndexC = (heatIndexF - 32) * 5/9;
+
+  return parseFloat(heatIndexC.toFixed(1));
+}
+
 const nowParameters = [
   't_2m:C',
-  // 'feels_like_2m:C',
   'weather_symbol_1h:idx',
   'relative_humidity_2m:p',
   'wind_speed_10m:ms',
   'precip_1h:mm',
   'uv:idx',
   'sunrise:sql',
-  'sunset:sql'
+  'sunset:sql',
+  'msl_pressure:hPa'
 ].join(',');
 
 const nowApiUrl = `https://api.meteomatics.com/now/${nowParameters}/${latitude},${longitude}/json`;
@@ -35,6 +54,7 @@ fetch(nowApiUrl, {
 .then(data => {
   const temperature = data.data.find(p => p.parameter === 't_2m:C').coordinates[0].dates[0].value;
   const humidity = data.data.find(p => p.parameter === 'relative_humidity_2m:p').coordinates[0].dates[0].value;
+
   const feelsLikeTemp = calculateHeatIndex(temperature, humidity);
   data.calculated_feels_like_C = feelsLikeTemp;
 
